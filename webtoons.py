6d19e058c508c72e3269ed718d4e8c9a4bb67cf8
somyajain <somyajain@gmail.com>
2016-05-04 17:15:46 +0530

    made four functions

--- webtoons.py
+++ webtoons.py
@@ -15,9 +15,48 @@ class Webtoons:
 
 
         self.s=ls.find_all(class_="subj")
-        count=1
+
+        released_list=[]
         for e in self.s:
-            print str(count) +' '+ e.get_text()
+            released_list.append(e.get_text())
+
+        return released_list
+
+    def new_releases_all(self):
+        released_all= self.new_releases()
+        count=1
+        for i in released_all:
+            print str(count)+ ' '+i
             count+=1
 
-    def top10_of_day(self)
+    def top10_of_day(self):
+        released=self.new_releases()
+        for i in range(0,10):
+            print str(i+1) + ' '+released[i]
+
+
+    def best_rated(self):
+        page=requests.get('http://www.webtoons.com/en/top')
+        soup=bs(page.content,'lxml')
+        ls=soup.find(class_="ranking_lst top")
+        subjall=ls.find_all(class_="subj")
+        count = 1
+        for i in subjall:
+            print str(count)+' ' +i.get_text()
+            count+=1
+
+    def top_in_genre(self,g):
+        page=requests.get('http://www.webtoons.com/en/genre')
+        soup=bs(page.content,'lxml')
+        gen=g.split(" ")
+        genre=''
+        for e in gen:
+            genre+="_"+e
+
+        ls=soup.find(class_="sub_title g"+genre)
+        ul=ls.findNextSibling('ul')
+        names=ul.find_all(class_="subj")
+        count=1
+        for i in names:
+            print= str(count)+' '+ i.get_text()
+            count+=1
